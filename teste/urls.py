03353cd0a4779234573e7be31609871ac9d56aeb
Raquel2050 <raquelcavalcante752@gmail.com>
2024-08-06 09:15:51 -0300

    commit de Raquel

--- teste/urls.py
+++ teste/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import brendha
+from .views import index, brendha, raquel
 
 urlpatterns = [
-    path('brendha/', brendha, name='brendha')
+    path('brendha/', brendha, name='brendha'),
+    path('raquel/', raquel, name='raquel'),
+    path('', index, name='index')
 ]
--- teste/views.py
+++ teste/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def brendha(request):
+    return render(request, 'brendha.html')
+def raquel(request):
+    return render(request, 'raquel.html')
+def index(request):
     return render(request, 'index.html')
